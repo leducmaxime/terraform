494661bef3829b3f1aef307d7255f28e8e0688bb
Maxime Leduc <leducmaxime@gmail.com>
2023-08-27 17:52:15 +0200

    aws build

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = "ami-04362390cf93a4a65"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-012852f2323dcafb9"]
+  subnet_id              = "subnet-0dc56f911d96805e9"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
