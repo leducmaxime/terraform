3224c711b386f1f557313c074a3ec4722699e975
Maxime Leduc <leducmaxime@gmail.com>
2023-08-28 15:49:38 +0200

    aws modules use

---
+++ learn-terraform-modules-use/terraform.tf
@@ -0,0 +1,19 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  cloud {
+    organization = "leducmaxime"
+    workspaces {
+      name = "learn-terraform-module-use"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.49.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
